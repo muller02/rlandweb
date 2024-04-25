b84cb4a30a58b1cbe70ad8d964156c2eecdad43b
daback <daback93@gmail.com>
2024-04-25 11:44:12 +0900

    react로 화면 갱신하기 with setState

--- src/main/resources/static/js/menu/list-react.js
+++ src/main/resources/static/js/menu/list-react.js
@@ -8,6 +8,32 @@
 
 // 주업무를 가지고있고 캡슐화가 필요하다면 class 정의
 class Main extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            list:[],
+            query:""
+        };
+    }
+
+    queryClickHandler(e){
+        e.preventDefault();
+        console.log("queryClick");
+    }
+
+    async componentDidMount(){    
+        let response = await fetch("/api/menus");
+        let list = await response.json();
+        // 필드와 변수의 이름이 같아야 생략, 적용이 가능
+        // 다를 경우 {list:menus}
+        this.setState({list});
+    }
+
+    componentDidUpdate(){
+
+    }
+
     render(){
         return <>
             <section className="menu-list">
@@ -32,8 +58,11 @@ class Main extends React.Component{
                             <form action="list" method="get">
                                 <fieldset>
                                     <legend className="d:none">이름 검색</legend>
-                                    <input className="query-input" type="text" placeholder="메뉴 검색" name="q"/>
-                                    <button className="icon icon-find">검색</button>
+                                    <input className="query-input" type="text" 
+                                        placeholder="메뉴 검색" name="q" 
+                                        value={this.state.query}
+                                        onChange={(e)=>{this.setState({query:e.target.value});}}/>
+                                    <button className="icon icon-find" onClick={this.queryClickHandler}>검색</button>
                                 </fieldset>
                             </form>
                         </section>
@@ -44,37 +73,47 @@ class Main extends React.Component{
                     <section className="menu-card-list" id="menu-card-list">
                         <h1 className="d:none">메뉴 목록</h1>
                         <div className="content fade">
-                            <section className="menu-card">
-                                <h1>
-                                    <a className="heading-3" href="detail.html">카페라떼1</a></h1>
-                                <h2 className="heading-2 font-weight:normal color:base-5">Cafe Latte</h2>
-                                <div className="price-block d:flex align-items:flex-end"><span className="font-weight:bold">4,500</span>원<span className="soldout-msg ml:auto mr:auto md:d:none">SOLD OUT</span></div>
-                                <div className="img-block">
-                                    <a href="detail.html?id=1">
-                                        <img src="/image/menu/8.jpg" />                                      
-                                    </a>
-                                </div>
-                                <div className="like-block d:flex justify-content:flex-end">
-                                    <a className="icon icon-heart icon-color:base-4"                                
-                                    >좋아요</a>
-                                    <span className="font-weight:bold ml:1">2</span>                                
-                                </div>
-                                <div>
-                                    <button>삭제</button>
-                                </div>
-                                <div className="pay-block d:flex">
-                                    {/* <!-- <a className="icon md:icon:none icon-cart icon-color:base-0 color:base-0 btn-type:icon btn-cart md:btn-type:text" href="">담기</a> --> */}
-                                    <form action="/cart/add-menu" method="post">
-                                        <input type="hidden" name="id"/>
-                                        
-                                        <button                                                                             
-                                            className="icon md:icon:none icon-cart icon-color:base-0 color:base-0 btn-type:icon btn-cart md:btn-type:text">
-                                            담기
-                                        </button>
-                                    </form>
-                                    <a className="icon md:icon:none icon-card icon-color:base-0 color:base-0 btn-type:icon btn-card md:btn-type:text">주문하기</a>
-                                </div>
-                            </section>                        
+
+                        {/* 반복문 레스고 */}
+                        {/* 코드블럭을 만들때 중괄호... (이렇게까지해야돼?) */}
+                        {
+                            //
+                            this.state.list.map( m =>
+
+                                <section className="menu-card">
+                                    <h1>
+                                        <a className="heading-3" href="detail.html">카페라떼1</a></h1>
+                                    <h2 className="heading-2 font-weight:normal color:base-5">Cafe Latte</h2>
+                                    <div className="price-block d:flex align-items:flex-end"><span className="font-weight:bold">4,500</span>원<span className="soldout-msg ml:auto mr:auto md:d:none">SOLD OUT</span></div>
+                                    <div className="img-block">
+                                        <a href="detail.html?id=1">
+                                            <img src="/image/menu/8.jpg" />                                      
+                                        </a>
+                                    </div>
+                                    <div className="like-block d:flex justify-content:flex-end">
+                                        <a className="icon icon-heart icon-color:base-4"                                
+                                        >좋아요</a>
+                                        <span className="font-weight:bold ml:1">2</span>                                
+                                    </div>
+                                    <div>
+                                        <button>삭제</button>
+                                    </div>
+                                    <div className="pay-block d:flex">
+                                        {/* <!-- <a className="icon md:icon:none icon-cart icon-color:base-0 color:base-0 btn-type:icon btn-cart md:btn-type:text" href="">담기</a> --> */}
+                                        <form action="/cart/add-menu" method="post">
+                                            <input type="hidden" name="id"/>
+                                            
+                                            <button                                                                             
+                                                className="icon md:icon:none icon-cart icon-color:base-0 color:base-0 btn-type:icon btn-cart md:btn-type:text">
+                                                담기
+                                            </button>
+                                        </form>
+                                        <a className="icon md:icon:none icon-card icon-color:base-0 color:base-0 btn-type:icon btn-card md:btn-type:text">주문하기</a>
+                                    </div>
+                                </section>  
+                            )
+                        } 
+
                         </div>
                     </section>
                 </div>
@@ -84,7 +123,7 @@ class Main extends React.Component{
                 <h1 className="d:none">페이저</h1>
                 <div>0</div>
                 <div>0</div>
-                <ul className="n-pager" style={{display:"flex", "justify-content": "center"}}>
+                <ul className="n-pager" style={{display:"flex", justifyContent: "center"}}>
                     
                     <li>
                         <span>이전</span>
@@ -119,6 +158,19 @@ class Main extends React.Component{
         </>
     }
 }
+
+//18버전 렌더링 방식
+const root = ReactDOM.createRoot(
+    document.querySelector("#main"));
+root.render(<Main/>);
+
+
+// 17버전 렌더링 방식
+// ReactDOM.render( 
+//     <Main/> , 
+//     document.querySelector("#main")
+// )
+
 
 // function Main(){
 //     return <>
@@ -230,13 +282,6 @@ class Main extends React.Component{
 //         </section>
 //     </>
 // }
-
-ReactDOM.render( 
-    <Main/> , 
-    document.querySelector("#main")
-)
-
-
 // ReactDOM.render(
 //     <span><HoHo/><HeHe/></span>, 
 //     document.querySelector("#test")
