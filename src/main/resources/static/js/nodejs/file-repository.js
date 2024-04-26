ea267d94c10db3183661df62853561fc00a3f439
daback <daback93@gmail.com>
2024-04-26 14:38:38 +0900

    npm 모듈 사용

--- src/main/resources/static/js/nodejs/file-repository.js
+++ src/main/resources/static/js/nodejs/file-repository.js
@@ -5,9 +5,19 @@ function findAll(path, {typeName, fileName}){
 
     if(typeName){
         
-        fileList = fs.readdirSync(path);
-        let listFiltered = fileList.filter((fileName)=>fileName.endsWith(".js"));
-        return listFiltered;
+        let list = fs
+        .readdirSync(path, {withFileTypes:true})
+        .filter((dirent)=>dirent.isDirectory())
+        .map((folder)=>folder.name);
+        // .filter((fileName)=>fileName.endsWith(".js"))
+        // .map((fileName)=>fileName.replace(".js", ".vs"))
+        // .reduce((pre, curItem)=>pre+curItem.length, 0);
+
+        // fileList = fs.readdirSync(path);
+        // let listFiltered = fileList.filter((fileName)=>fileName.endsWith(".js"));
+        // let listMapped = listFiltered.map((fileName)=>fileName.replace(".js", ".vs"));
+        // return listMapped;
+        return list;
     }
     return fileList;
 }
--- src/main/resources/static/js/nodejs/first.js
+++ src/main/resources/static/js/nodejs/first.js
@@ -1,6 +1,13 @@
-console.log("Hello NodeJS");
+const fs = require('fs');
 
+let newlec = require("newlec-hello");
 let repository = require("./file-repository.js");
-console.log(repository.findAll("./",{
-    typeName:".js"
-}));
+
+newlec.hello();
+
+let dirList = repository.findAll("../",{
+                                typeName:".js"
+                            });
+
+// let csv = dirList.join();
+// fs.writeFileSync("./foldList.txt", csv);
