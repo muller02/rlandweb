c26f60e25784a3a2807aaade83d455a8b482c4fd
daback <daback93@gmail.com>
2024-04-30 10:47:28 +0900

    express -> ejs 이미지 업로드까지

--- src/main/resources/static/js/nodejs/project/index.js
+++ src/main/resources/static/js/nodejs/project/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 const server = express();
+
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+server.use(express.static(path.join(__dirname, "public")));
+
+
 server.listen(82);
 
 server
 .route("/index")
 .get((req,res)=>{
-    
+    res.render("index.ejs", {test:"Hello"});
 })
 
 // server.use("/index", (req, res)=>{
