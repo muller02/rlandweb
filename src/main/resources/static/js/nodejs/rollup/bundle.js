6f1797679a27fbe40fca7c0a86f00910c728391b
daback <daback93@gmail.com>
2024-05-01 20:20:46 +0900

    rollup 번들 패키징

---
+++ src/main/resources/static/js/nodejs/rollup/bundle.js
@@ -0,0 +1,5 @@
+function test1(){
+    console.log("HEll");
+}
+
+test1();
---
+++ src/main/resources/static/js/nodejs/rollup/target/bundle.js
@@ -0,0 +1,6 @@
+function test1$1(){
+    console.log("HEll");
+}function test1(){
+    console.log("HEll2222");
+}test1$1();
+test1();
